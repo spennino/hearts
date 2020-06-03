270cb0ad6e702c3b557a5ef1b67ddc45ce32b0ea
spennino <pennino.sean@gmail.com>
2020-06-02 22:19:23 -0700

    Change to server

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const { promisify } = require("util")
 const redisGet = promisify(redis.get).bind(redis)
 const redisSet = promisify(redis.set).bind(redis)
 
-const app = express()
+const server = express()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')))
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 // Put all API endpoints under '/api'
-app.get('/api/game/create', (req, res) => {
+server.get('/api/game/create', (req, res) => {
   let gameCode = shortid.generate()
   if (process.env.NODE_ENV === 'development') {
     res.json('sr8kOcHM9')
@@ -30,7 +30,7 @@ app.get('/api/game/create', (req, res) => {
   }
 })
 
-app.get('/api/game/get/:gameCode', jsonParser, (req, res) => {
+server.get('/api/game/get/:gameCode', jsonParser, (req, res) => {
   redisGet(req.params.gameCode).then(gameState => {
     if (!gameState) {
       res.sendStatus(404)
@@ -40,7 +40,7 @@ app.get('/api/game/get/:gameCode', jsonParser, (req, res) => {
   })
 })
 
-app.post('/api/game/update', jsonParser, (req, res) => {
+server.post('/api/game/update', jsonParser, (req, res) => {
   redisSet(req.body.gameCode, JSON.stringify(req.body)).then(status => {
     res.sendStatus(200)
   })
@@ -48,13 +48,13 @@ app.post('/api/game/update', jsonParser, (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
-app.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000)
 
-const wss = new Server({ app });
+const wss = new Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
